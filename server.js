598d1930276638b93f14909f79322dd737cb7392
Dragonsworld47 <167680752+Dragonsworld47@users.noreply.github.com>
2025-09-29 14:36:47 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -54,6 +54,3 @@ const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
-server.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
